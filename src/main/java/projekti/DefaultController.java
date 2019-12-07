9935d0ec3d5c81ea0892e19f02b902f14243d96a
Salla Koskinen <sallakos@mbnet.fi>
2019-12-07 16:09:21 +0200

    lisätty muutama tyyppi valmiiksi tietokantaan

--- src/main/java/projekti/DefaultController.java
+++ src/main/java/projekti/DefaultController.java
@@ -19,15 +19,31 @@ public class DefaultController {
     @PostConstruct
     public void init() {
         
-        AccountData user1 = new AccountData("aku", "Aku Ankka", "aku");
-        AccountData user2 = new AccountData("iinez", "Iines Ankka", "iinez");
+        AccountData user1 = new AccountData("Aku Ankka", "aku", "aku");
+        AccountData user2 = new AccountData("Iines Ankka", "iinez", "iinez");
+        AccountData user3 = new AccountData("Mikki Hiiri", "mikkihiiri", "mikkihiiri");
+        AccountData user4 = new AccountData("Hessu Hopo", "hessuh", "hessuh");
+        AccountData user5 = new AccountData("Hannu Hanhi", "hanzuu", "hanzuu");
+        AccountData user6 = new AccountData("Minni Hiiri", "minnie", "minnie");
+        
 
         if (accountService.findByUsername(user1.getUsername()) == null) {
             accountService.register(user1);
         }
-        
         if (accountService.findByUsername(user2.getUsername()) == null) {
             accountService.register(user2);
+        }
+        if (accountService.findByUsername(user3.getUsername()) == null) {
+            accountService.register(user3);
+        }
+        if (accountService.findByUsername(user4.getUsername()) == null) {
+            accountService.register(user4);
+        }
+        if (accountService.findByUsername(user5.getUsername()) == null) {
+            accountService.register(user5);
+        }
+        if (accountService.findByUsername(user6.getUsername()) == null) {
+            accountService.register(user6);
         }
         
     }
