e29df24d42226fb6263ac3cc9d64443181957ded
Salla Koskinen <sallakos@mbnet.fi>
2019-12-09 18:45:20 +0200

    ensimmäiset testit

--- src/test/java/projekti/ProjektiTest.java
+++ src/test/java/projekti/ProjektiTest.java
@@ -1,11 +1,52 @@
 package projekti;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
-public class ProjektiTest {
+@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ProjektiTest extends org.fluentlenium.adapter.junit.FluentTest {
 
+    @LocalServerPort
+    private Integer port;
+    
+    @Autowired
+    AccountRepository accountRepo;
+    
     @Test
-    public void noTests() {
-        // projektia varten ei ole automaattisia testejä
+    public void canRegister() {
+        
+        assertTrue(accountRepo.findByUsername("rolle") == null);
+        
+        goTo("http://localhost:" + port + "/rekisteroidy");
+        find("#name").fill().with("Rolle Ryöväri");
+        find("#username").fill().with("rolle");
+        find("#password").fill().with("salasana");
+        find("form").first().submit();
+
+        assertTrue(accountRepo.findByUsername("rolle") != null);
+        
     }
+    
+    @Test
+    public void canLogIn() {
+        
+        goTo("http://localhost:" + port + "/login");
+        find("#username").fill().with("rolle");
+        find("#password").fill().with("salasana");
+        find("form").first().submit();
+        goTo("http://localhost:" + port + "/rolle");
+        
+        assertTrue(pageSource().contains("Rolle Ryöväri"));
+        
+    }
+    
 }
